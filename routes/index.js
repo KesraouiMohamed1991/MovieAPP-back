10ae0ba056967aa2dc30f6a31a6e6b6c51f75709
kesraoui mohamed <kesraoui15003@gmail.com>
2023-11-15 14:45:49 +0100

    yes

--- routes/index.js
+++ routes/index.js
@@ -8,19 +8,15 @@ const MOVIE_API_URL = 'https://api.themoviedb.org/3/movie/popular';
 router.get("/movies", async (req, res) => {
     try {
         const response = await fetch(`${MOVIE_API_URL}?api_key=${API_KEY}`);
-
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
-
         const data = await response.json();
         res.json({ movies: data });
-
 
     } catch (error) {
         console.error('Error fetching movies:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-
 module.exports = router;
